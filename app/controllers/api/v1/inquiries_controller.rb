45f36f58a1212e30c2abe13b468bb5c6ed0d2973
Erin Cola <colaerin@yahoo.com>
2021-03-15 20:54:32 -0400

    updated inquiry controller and routes

--- app/controllers/api/v1/inquiries_controller.rb
+++ app/controllers/api/v1/inquiries_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::InquiriesController < ApplicationController
+    before_action :find_inquiry, only: [:update]
+
+  def index
+    @inquiries = Inquiry.all
+    render json: @inquiries
+  end
+
+  def update
+    @inquiry.update(inquiry_params)
+    if @inquiry.save
+      render json: @inquiry, status: :accepted
+    else
+      render json: { errors: @inquiry.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def inquiry_params
+    params.permit(:msg, :foster_id, :saves)
+  end
+
+  def find_inquiry
+    @inquiry = Inquiry.find(params[:id])
+  end
 end
